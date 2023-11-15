612e4a491cee212d37319134c096fb2307dcf1b0
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-15 23:19:37 +0600

    shadcn component ui updated

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -3,10 +3,10 @@ import { tv, type VariantProps } from "tailwind-variants";
 import type { Button as ButtonPrimitive } from "bits-ui";
 
 const buttonVariants = tv({
-	base: "inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+	base: "inline-flex items-center justify-center rounded-md text-lg font-semibold capitalize whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
 	variants: {
 		variant: {
-			default: "bg-primary text-primary-foreground hover:bg-primary/90",
+			default: "bg-primary text-primary-foreground hover:bg-primary/90 text-xl  ",
 			destructive:
 				"bg-destructive text-destructive-foreground hover:bg-destructive/90",
 			outline:
@@ -17,7 +17,7 @@ const buttonVariants = tv({
 			link: "text-primary underline-offset-4 hover:underline"
 		},
 		size: {
-			default: "h-10 px-4 py-2",
+			default: "p-3 px-5 md:p-5 md:px-8",
 			sm: "h-9 rounded-md px-3",
 			lg: "h-11 rounded-md px-8",
 			icon: "h-10 w-10"
