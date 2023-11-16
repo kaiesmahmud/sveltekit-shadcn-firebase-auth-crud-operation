f0d42caf995d2f5c1b7d6d1302aff0a39dd58096
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-16 20:52:44 +0600

    authentication function added

--- src/lib/components/Authenticate.svelte
+++ src/lib/components/Authenticate.svelte
@@ -1,17 +1,44 @@
 <script lang="ts">
-import { Button } from "$lib/components/ui/button";
-import { Input } from "$lib/components/ui/input";
-import { Label } from "$lib/components/ui/label";
+    import Icon from '@iconify/svelte';
+    import { Button } from "$lib/components/ui/button";
+    import { Input } from "$lib/components/ui/input";
+    import { Label } from "$lib/components/ui/label";
+    import { authHandler } from "../store/store";
 
 
-let email = "";
-let password = "";
-let confirmpass = "";
-let register = false ;
-let inputStyles = "w-[100%] p-5 rounded bg-white/50 focus-within:border-none"
-const changeRegister = () => {
-    register = !register
-}
+    let email = "";
+    let password = "";
+    let confirmpass = "";
+    let register = true ;
+    let error = false ;
+    let authenticating = false ;
+
+    let inputStyles = "w-[100%] p-5 rounded bg-white/50 focus-within:border-none"
+
+    const changeRegister = () => {
+        register = !register
+    }
+
+    const handleAuthenticate = async ()=>{
+        if(authenticating){return ;}
+
+        if(!email && !password || (register && !confirmpass)){
+            error = true ;
+            return ;
+        }
+        authenticating= true ;
+        try {
+            if(!register ){
+                await authHandler.login(email,password);
+            }else{
+                await authHandler.signup(email,password);
+    
+            }
+        } catch (error) {
+            console.log("Error in Auth,", error)
+            authenticating = false ;
+        }
+    }
 </script>
 
 <div class="w-[90%] md:w-[70%] lg:w-[50%] bg-white/10 rounded p-5 flex flex-col items-center justify-center gap-10 pb-20 pt-10">
@@ -21,7 +48,7 @@ const changeRegister = () => {
         <h1 class=" text-green-500">Login</h1>
     {/if}
     
-    <form  class="flex flex-col items-center justify-center gap-3 w-2/3">
+    <form on:submit={handleAuthenticate}  class="flex flex-col items-center justify-center gap-3 w-2/3">
         <Label for="email" class="w-[100%]  font-bold" >Email:</Label>
         <Input bind:value={email} class={inputStyles}  type="email" name="email" id="email" required />
         <Label for="password" class="w-[100%] font-bold ">Password:</Label>
@@ -29,9 +56,21 @@ const changeRegister = () => {
         {#if register}
             <Label for="checkpassword" class="w-[100%] font-bold ">Confirm Password:</Label>
             <Input bind:value={confirmpass} class={inputStyles}  type="password" name="checkpassword" id="checkpassword" required />            
-            <Button type="submit" class="w-full mt-5">Register</Button>
+            <Button type="submit" class="w-full mt-5">
+                {#if authenticating}
+                    <Icon icon="eos-icons:bubble-loading" />
+                {:else}
+                     Register
+                {/if}
+            </Button>
         {:else}
-            <Button type="submit" class="w-full mt-5">login</Button>
+            <Button  type="submit" class="w-full mt-5">
+                {#if authenticating}
+                    <Icon icon="eos-icons:bubble-loading" />
+                {:else}
+                     login
+                {/if}
+            </Button>
         {/if}
         
         
